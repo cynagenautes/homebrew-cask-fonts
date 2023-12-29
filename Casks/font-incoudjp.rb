f43c27cddb99fc97a5d3d37e95f0acd28a8371c4
cynagenautes <38454938+cynagenautes@users.noreply.github.com>
2023-12-30 03:06:39 +0900

    Add IncoUDJP

---
+++ Casks/font-incoudjp.rb
@@ -0,0 +1,17 @@
+cask "font-incoudjp" do
+  version "0.10"
+  sha256 :no_check
+
+  url "https://structure.kais.kyoto-u.ac.jp/lab/IncoUDJP010b.zip"
+  name "IncoUDJP"
+  homepage "https://structure.kais.kyoto-u.ac.jp/lab/photo.html#IncoUDJP"
+
+  font "IncoUDJP010b/IncoUDJP-Regular-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJP-Bold-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJPterm-Regular-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJPterm-Bold-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJPtiny-Regular-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJPtiny-Bold-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJPterm2-Regular-km10p-uh.ttf"
+  font "IncoUDJP010b/IncoUDJPterm2-Bold-km10p-uh.ttf"
+end
